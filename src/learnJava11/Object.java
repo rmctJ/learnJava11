c99e1439df07cf0330e60032a62908eb6091984a
rmctJ <sD3zyC@126.com>
2019-02-26 20:59:57 +0800

    First commit

---
+++ src/learnJava11/Object.java
@@ -0,0 +1,10 @@
+package learnJava11;
+
+public class Object {
+
+	public static void main(String[] args) {
+		System.out.println("version = " + Runtime.version().toString());
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Think
+ *
+ */
+module learnJava11 {
+}
